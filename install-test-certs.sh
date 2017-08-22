963f660b774943c37631d0a73f4e71b0dd283536
Benjamin Gaunitz <gaunitz@wifa.uni-leipzig.de>
2017-08-22 16:12:10 +0200

    - changed logstash healthcheck - updated install-test script

--- install-test-certs.sh
+++ install-test-certs.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 CERT_DIR="cert-scripts"
 
-mkdir -p /example/filebeat/certs
-cp /certs/ca/ca.crt /example/filebeat/certs/
-cp /certs/clients/filebeat1.* /example/filebeat/certs/
+mkdir -p example/filebeat/certs
+cp certs/ca/ca.crt example/filebeat/certs/
+cp certs/clients/filebeat1.* example/filebeat/certs/
